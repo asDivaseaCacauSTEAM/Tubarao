c5eecbab4b5468842e995f04d53d6229a0aa95b7
asDivaseaCacauSTEAM <a2572@escolaglobal.org>
2026-05-29 16:24:16 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,55 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+....................
+....................
+.......888..........
+.......8888.........
+.......88888........
+........88888.......
+.........88888888...
+88......888888f888..
+888...88888888888888
+.8888888888f81f88888
+.8888888888f88888888
+..888888888888888888
+.88888888888888222..
+88888888888888888...
+88....98888...888...
+.......99......8....
+....................
+....................
+....................
+....................
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
