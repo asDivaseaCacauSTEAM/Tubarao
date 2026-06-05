2fa880f60562432d61ff81f74d7d4a420e29acd4
asDivaseaCacauSTEAM <a2572@escolaglobal.org>
2026-06-05 16:43:55 +0100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+let Ulisses: Sprite = null
+controller.moveSprite(Ulisses, 100, 5)
+Ulisses = sprites.create(assets.image`Ulisses`, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level0`)
+scene.cameraFollowSprite(Ulisses)
+controller.moveSprite(Ulisses)
